94984fd9a9cc90d95f38f0b241f01a81ae76ca02
xxyrnn <xyrnproject@gmail.com>
2025-12-06 23:11:11 +0100

    added check on path

--- refind.py
+++ refind.py
@@ -1,20 +1,27 @@
-from pathlib import Path, PosixPath
 import argparse
-import sys
 import re
+import sys
+from pathlib import Path
 
-def list_files(p: PosixPath):
+
+def list_files(p: Path):
     return [file for file in p.rglob("*") if file.is_file()]
 
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser("refind.py",
-        description="Search for the given pattern in all the files within the given path and its subdirectories"
+    parser = argparse.ArgumentParser(
+        "refind.py",
+        description="Search for the given pattern in all the files within the given path and its subdirectories",
     )
-    parser.add_argument("path", help="path to look for files")
+    parser.add_argument("path", help="directory to list for files")
     parser.add_argument("pattern", help="pattern to search in the files")
 
     args = parser.parse_args()
     p = Path(args.path)
+
+    if p.is_file():
+        sys.exit("[!] ERROR: path must be a directory")
+
     files = list_files(p)
 
     for file in files:
@@ -24,4 +31,3 @@ if __name__ == "__main__":
                     print(file)
         except UnicodeDecodeError:
             continue
-
